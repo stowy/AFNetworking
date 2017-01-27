b6eb76027079c4ffaf2bc7135cd6e8e63798aca5
Sam Stow <sstow@groupon.com>
2017-01-27 10:43:55 -0800

    Added carthage support

--- AFNetworking/AFNetworking.h
+++ AFNetworking/AFNetworking.h
@@ -19,9 +19,14 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-
 #import <Foundation/Foundation.h>
 #import <Availability.h>
+
+//! Project version number for AFNetworking.
+FOUNDATION_EXPORT double AFNetworkingVersionNumber;
+
+//! Project version string for AFNetworking.
+FOUNDATION_EXPORT const unsigned char AFNetworkingVersionString[];
 
 #ifndef _AFNETWORKING_
     #define _AFNETWORKING_
